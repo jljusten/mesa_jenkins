6b2c42ef21aec049a36412de048a090cf7ffa19f
Mark Janes <mark.a.janes@intel.com>
2016-02-25 14:01:20 -0800

    empty test configs

--- piglit-test/build.py
+++ piglit-test/build.py
@@ -1,27 +1,8 @@
 #!/usr/bin/python
 
-import sys, os, argparse
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), ".."))
-import build_support as bs
-
 class SlowTimeout:
-    def __init__(self):
-        self.hardware = bs.Options().hardware
 
     def GetDuration(self):
-        if self.hardware == "bsw":
-            return 120
-        if self.hardware == "byt":
-            return 120
-        if self.hardware == "bxt":
-            return 120
-        if self.hardware == "g33":
-            return 120
-        if self.hardware == "g965":
-            return 50
-        # all other test suites finish in 10 minutes or less.
-        # TODO: put back to 25 when curro's regression is fixed
-        return 40
         
 # add the --piglit_test option to the standard options.  Parse the
 # options, and strip the piglit_test so the options will work as usual
